eff4f031d9ab23c750f12c3d1715f6c0684d87f7
Brian McKenna <D00197352@student.dkit.ie>
2022-12-18 17:43:41 +0000

    Added functions from other referenced solution with modifications

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import keras
@@ -18,6 +19,12 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 import pickle
 import pandas as pd
+
+# declare constants
+NUM_CLASSES = 200
+NUM_IMAGES_PER_CLASS = 500
+IMG_SIZE = 64
+
 
 # directories & files paths
 test_directory = "tiny-imagenet-200/test"
@@ -52,3 +59,79 @@ def pre_process_img(img):
     img = equalise(img)
     img = img / 255
     return img
+
+
+# Following solution from the reference with modifications:
+# ***************************************************************************************/
+# *    Title: Train_ResNet_On_Tiny_ImageNet
+# *    Author: Giri, S
+# *    Date: Mar 29, 2019
+# *    Code version: 1.0
+# *    Availability: https://github.com/sonugiri1043/Train_ResNet_On_Tiny_ImageNet
+# ***************************************************************************************/
+# (Version 2.0) [Source code]. https://github.com/sonugiri1043/Train_ResNet_On_Tiny_ImageNet/blob/master/Train_ResNet_On_Tiny_ImageNet.ipynb
+
+
+# get class ids from text file
+def get_id_dictionary():
+    id_dict = {}
+    for i, line in enumerate(open(wnids_file, 'r')):
+        id_dict[line.replace('\n', '')] = i
+    return id_dict
+
+
+def get_class_to_id_dict():
+    id_dict = get_id_dictionary()
+    all_classes = {}
+    result = {}
+    for i, line in enumerate(open(words_file, 'r')):
+        n_id, word = line.split('\t')[:2]
+        all_classes[n_id] = word
+    for key, value in id_dict.items():
+        result[value] = (key, all_classes[key])
+    return result
+
+
+def get_data(id_dict):
+    print('starting loading data')
+    train_data, test_data = [], []
+    train_labels, test_labels = [], []
+    t = time.time()
+    for key, value in id_dict.items():
+        train_data += [plt.imread(train_directory + '/{}/images/{}_{}.JPEG'.format(key, key, str(i))) for i in
+                       range(NUM_CLASSES)]
+        train_labels_ = np.array([[0] * NUM_CLASSES] * NUM_IMAGES_PER_CLASS)
+        train_labels_[:, value] = 1
+        train_labels += train_labels_.tolist()
+
+    for line in open(val_directory + '/val_annotations.txt'):
+        img_name, class_id = line.split('\t')[:2]
+        test_data.append(plt.imread(val_directory + '/images/{}'.format(img_name)))
+        test_labels_ = np.array([[0] * NUM_CLASSES])
+        test_labels_[0, id_dict[class_id]] = 1
+        test_labels += test_labels_.tolist()
+
+    print('finished loading data, in {} seconds'.format(time.time() - t))
+    return np.array(train_data), np.array(train_labels), np.array(test_data), np.array(test_labels)
+
+
+train_data, train_labels, test_data, test_labels = get_data(get_id_dictionary())
+
+train_data = np.array(list(map(pre_process_img, train_data)))
+test_data = np.array(list(map(pre_process_img, test_data)))
+
+assert (train_data.shape[0] == train_labels.shape[
+    0]), "The training set does not have the same number of data points and labels"
+assert (train_data.shape[0] == train_labels.shape[0]), "The test set does not have the same number of data points and labels"
+
+displayImage = train_data[0]
+plt.imshow(displayImage)
+plt.axis("off")
+plt.show()
+
+# print(img.shape)
+#
+# print("train data shape: ", train_data.shape)
+# print("train label shape: ", train_labels.shape)
+# print("test data shape: ", test_data.shape)
+# print("test_labels.shape: ", test_labels.shape)
